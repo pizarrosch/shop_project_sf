149392423b8c2e9a21efaf513fa5c4b306a9a2b7
pizarrosch <bayenrshc89@gmail.com>
2023-10-02 01:16:35 +0200

    Add normalize.css and styles to the project. Get and display images from images table from the DB

--- Shop_admin/index.ts
+++ Shop_admin/index.ts
@@ -1,5 +1,6 @@
 import express, { Express } from "express";
 import {adminProductsRouter} from "./controllers/products.controllers";
+import layout from "express-ejs-layouts";
 
 export default function (): Express {
     const app = express();
@@ -8,6 +9,8 @@ export default function (): Express {
     app.set('views', 'Shop_admin/views')
 
     app.use(express.json());
+    app.use(layout);
+    app.use(express.static(__dirname + "/public"));
     app.use("/", adminProductsRouter);
 
     return app;
